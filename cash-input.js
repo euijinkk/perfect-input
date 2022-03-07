61d16fec2a027aefa0eadd65cd9842825e0454d0
gytks4 <gytks4@naver.com>
2022-03-07 15:28:32 +0900

    chore: 주석 설명 추가

--- cash-input.js
+++ cash-input.js
@@ -2,7 +2,6 @@ const $cashInput = document.getElementById("cash-input");
 
 const REGEXP = {
   NUMBER: /[0-9]/,
-  NOT_NUMBER_IN_NUMBER_INPUT: /[e.\-+]{,1}/,
   NOT_NUMBERorKOREAN: /[^0-9ㄱ-ㅎㅏ-ㅣ가-힣]/,
   NOT_NUMBER: /[^0-9]/g,
   KOREAN: /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/,
@@ -16,15 +15,19 @@ const possibleKey = ["a", "c,", "v", "x", "r", "z"];
 let valueAfterKeyDown = "";
 
 $cashInput.addEventListener("keydown", (e) => {
+  // Command/Ctrl 과 다른 알파벳을 함께 눌렀을 때
   if ((e.metaKey || e.ctrlKey) && possibleKey.includes(e.key)) {
     return;
   }
 
+  // 숫자나 한글이 아닌 문자가 입력되었을 때 (특수문자, 영어 등)
+  // Backspace, Meta, Tab, Shift 등을 인식하기 위해서, e.key의 길이를 validate
   if (REGEXP.NOT_NUMBERorKOREAN.test(e.key) && e.key.length === 1) {
     e.preventDefault();
     return;
   }
 
+  // 한글이 입력되었을 때, 임시변수에 저장함.
   if (REGEXP.KOREAN.test(e.key)) {
     valueAfterKeyDown = e.target.value;
     return;
@@ -34,24 +37,29 @@ $cashInput.addEventListener("keydown", (e) => {
 $cashInput.addEventListener("input", (e) => {
   const value = e.target.value;
 
+  // 비어있을 때, removeCommaInNumber(value).toLocaleString()를 실행하지 않도록함.
   if (value === "") {
     return;
   }
 
+  // 한글이 입력되었을 때, keydown에서 임시변수로 저장해둔 변수값으로 대치함.
   if (REGEXP.KOREAN.test(value)) {
     e.target.value = valueAfterKeyDown;
     return;
   }
 
+  // emoji 입력을 제한함
   if (REGEXP.EMOJI.test(value)) {
     e.target.value = value.replace(REGEXP.EMOJI, "");
     return;
   }
 
+  // 3자리 수마다 comma 를 더해줌
   e.target.value = removeCommaInNumber(value).toLocaleString();
 });
 
 $cashInput.addEventListener("paste", (e) => {
+  // 숫자가 아닌 것을 모두 ''으로 대체해줌
   if (REGEXP.NOT_NUMBER.test(e.clipboardData.getData("Text"))) {
     const extractedNumber = Number(
       e.clipboardData.getData("Text").replace(REGEXP.NOT_NUMBER, "")
--- number-input.js
+++ number-input.js
@@ -6,6 +6,8 @@ const REGEXP = {
 };
 
 $numberInput.addEventListener("keydown", (e) => {
+  // e, -, + 입력을 제한
+  // Backspace, Meta 등을 인식하기 위해서, e.key의 길이를 validate
   if (REGEXP.NOT_NUMBER_IN_NUMBER_INPUT.test(e.key) && e.key === 1) {
     e.preventDefault();
     return;
@@ -15,6 +17,7 @@ $numberInput.addEventListener("keydown", (e) => {
 $numberInput.addEventListener("paste", (e) => {
   const copiedText = e.clipboardData.getData("Text");
 
+  // 붙여넣기 된 텍스트에서 e, -, + 를 제외한 후 입력
   if (REGEXP.NOT_NUMBER.test(copiedText)) {
     const extractedNumber = copiedText.replace(REGEXP.NOT_NUMBER, "");
     e.target.value = extractedNumber;

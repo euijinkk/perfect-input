c5939c39ef2393a614a3a801dbcf9592075fda6d
gytks4 <gytks4@naver.com>
2022-03-07 15:12:22 +0900

    feat: number input에 e, -, + 제한

--- number-input.js
+++ number-input.js
@@ -1,1 +1,12 @@
 const $numberInput = document.getElementById("number-input");
+
+const REGEXP = {
+  NOT_NUMBER_IN_NUMBER_INPUT: /^[e\-+]$/,
+};
+
+$numberInput.addEventListener("keydown", (e) => {
+  if (REGEXP.NOT_NUMBER_IN_NUMBER_INPUT.test(e.key)) {
+    e.preventDefault();
+    return;
+  }
+});

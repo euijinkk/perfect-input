c1c0f8b49140e29d341af0cbf823daf7b337e455
gytks4 <gytks4@naver.com>
2022-03-07 11:21:39 +0900

    feat: 숫자-한글 제외 입력 제한

--- cash-input.js
+++ cash-input.js
@@ -1,1 +1,11 @@
+import { REGEXP } from "./regexp.js";
+
 const $cashInput = document.getElementById("cash-input");
+
+$cashInput.addEventListener("keydown", (e) => {
+  console.log("e.key", e.key);
+  if (REGEXP.NOT_NUMBERorKOREAN.test(e.key) && e.key.length === 1) {
+    e.preventDefault();
+    return;
+  }
+});

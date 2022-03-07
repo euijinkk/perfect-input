f2a144e8c8f2ad91574f4b404f670a8ddbd275cc
gytks4 <gytks4@naver.com>
2022-03-07 15:22:41 +0900

    feat: number input에 paste 시 e, -, + 제한

--- number-input.js
+++ number-input.js
@@ -1,11 +1,23 @@
 const $numberInput = document.getElementById("number-input");
 
 const REGEXP = {
-  NOT_NUMBER_IN_NUMBER_INPUT: /^[e\-+]$/,
+  NOT_NUMBER_IN_NUMBER_INPUT: /[e\-+]/,
+  NOT_NUMBER: /[^0-9]/g,
 };
 
 $numberInput.addEventListener("keydown", (e) => {
-  if (REGEXP.NOT_NUMBER_IN_NUMBER_INPUT.test(e.key)) {
+  if (REGEXP.NOT_NUMBER_IN_NUMBER_INPUT.test(e.key) && e.key === 1) {
+    e.preventDefault();
+    return;
+  }
+});
+
+$numberInput.addEventListener("paste", (e) => {
+  const copiedText = e.clipboardData.getData("Text");
+
+  if (REGEXP.NOT_NUMBER.test(copiedText)) {
+    const extractedNumber = copiedText.replace(REGEXP.NOT_NUMBER, "");
+    e.target.value = extractedNumber;
     e.preventDefault();
     return;
   }

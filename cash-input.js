c83a37471872db0f030853c72a2327fcb4b41ad9
gytks4 <gytks4@naver.com>
2022-03-07 11:31:10 +0900

    feat: 한글 입력 제한

--- cash-input.js
+++ cash-input.js
@@ -2,10 +2,24 @@ import { REGEXP } from "./regexp.js";
 
 const $cashInput = document.getElementById("cash-input");
 
+let valueAfterKeyDown = "";
+
 $cashInput.addEventListener("keydown", (e) => {
-  console.log("e.key", e.key);
   if (REGEXP.NOT_NUMBERorKOREAN.test(e.key) && e.key.length === 1) {
     e.preventDefault();
+    return;
+  }
+
+  if (REGEXP.KOREAN.test(e.key)) {
+    valueAfterKeyDown = e.target.value;
+    return;
+  }
+});
+
+$cashInput.addEventListener("input", (e) => {
+  const value = e.target.value;
+  if (REGEXP.KOREAN.test(value)) {
+    e.target.value = valueAfterKeyDown;
     return;
   }
 });

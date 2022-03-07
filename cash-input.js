ed16b7d916ff32bd7a5faa272dd56545cf694047
gytks4 <gytks4@naver.com>
2022-03-07 11:53:51 +0900

    feat: command, ctrl Key 가능하게 만들기

--- cash-input.js
+++ cash-input.js
@@ -4,7 +4,14 @@ const $cashInput = document.getElementById("cash-input");
 
 let valueAfterKeyDown = "";
 
+// cmd + 알파벳 - 전체선택(a) 복사(c) 붙여넣기(v) 자르기(x) 새로고침(r) 되돌리기(z)
+const possibleKey = ["a", "c,", "v", "x", "r", "z"];
+
 $cashInput.addEventListener("keydown", (e) => {
+  if ((e.metaKey || e.ctrlKey) && possibleKey.includes(e.key)) {
+    return;
+  }
+
   if (REGEXP.NOT_NUMBERorKOREAN.test(e.key) && e.key.length === 1) {
     e.preventDefault();
     return;
